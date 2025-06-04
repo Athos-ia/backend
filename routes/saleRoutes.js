e7a579c9fc97315e3579fc7b93f14697125c616e
athosguariza <athosguariza@gmail.com>
2025-06-04 10:01:26 -0300

    Add Swagger docs for additional routes

--- routes/saleRoutes.js
+++ routes/saleRoutes.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const saleController = require('../controllers/saleController');
 const auth = require('../middleware/authMiddleware');
 
+/**
+ * @swagger
+ * /api/sales:
+ *   get:
+ *     summary: Retrieve all sales
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of sales
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.get('/', auth, saleController.getAll);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,23 @@ const userController = require('../controllers/userController');
 const auth = require('../middleware/authMiddleware');
 const permissoes = require('../middleware/permissoesMiddleware');
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retrieve all users (admin only)
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of users
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       500:
+ *         description: Server error
+ */
 router.get('/', auth, permissoes('admin'), userController.getAll);
 
 module.exports = router;
