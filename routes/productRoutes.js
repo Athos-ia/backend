745e404e086baee1365ab5896d3589601598d395
athosguariza <athosguariza@gmail.com>
2025-06-04 09:47:19 -0300

    Add Swagger documentation

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,35 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const auth = require('../middleware/authMiddleware');
 
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Retrieve all products
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of products
+ *   post:
+ *     summary: Create a product
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Created product
+ */
 router.get('/', auth, productController.getAll);
 router.post('/', auth, productController.create);
 
